774d9b5175bb97d3e00485fb094d0cca7a0c1f5d
Deniz Can DİNÇ <deniz@agencylook.com>
2021-01-17 02:22:16 +0300

    updated particles.js app.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -20,10 +20,10 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 600,
+        value: 400,
         density: {
           enable: true,
-          value_area: 700
+          value_area: 800
         }
       },
       color: {
